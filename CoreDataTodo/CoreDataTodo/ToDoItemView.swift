1ee9ef0d9a554641a800c7226f9cc4e204630d8f
Pratham <62070812+prathami1@users.noreply.github.com>
2021-01-22 09:26:19 -0500

    First Commit 🔥
    First Commit

---
+++ CoreDataTodo/CoreDataTodo/ToDoItemView.swift
@@ -0,0 +1,28 @@
+//
+//  ToDoItemView.swift
+//  CoreDataTodo
+//
+//  Created by Pratham Inamdar on 1/21/21.
+//
+
+import SwiftUI
+
+struct ToDoItemView: View {
+    var title:String = ""
+    var createdAt:String = ""
+    
+    var body: some View {
+        HStack{
+            VStack(alignment: .leading){
+                Text(title).font(.headline)
+                Text(createdAt).font(.caption)
+            }
+        }
+    }
+}
+
+struct ToDoItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoItemView(title: "My great todo", createdAt: "Today")
+    }
+}
